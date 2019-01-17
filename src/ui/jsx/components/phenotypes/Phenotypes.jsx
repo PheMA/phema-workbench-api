c0da9ee332cbea666de8655f4dc3040e1700b8d7
Pascal Brandt <psbrandt@gmail.com>
2019-01-16 20:25:35 -0800

    🧙🏻‍♀️ Finish fake upload.

--- src/ui/jsx/components/phenotypes/Phenotypes.jsx
+++ src/ui/jsx/components/phenotypes/Phenotypes.jsx
@@ -3,42 +3,39 @@ import PropTypes from "prop-types";
 import { Toaster, Position, Dialog, Button, Icon } from "@blueprintjs/core";
 import Dropzone from "react-dropzone";
 import ActionHeader from "../common/ActionHeader.jsx";
+import localForage from "localforage";
+import numeral from "numeral";
+import moment from "moment";
+
+localForage.config({
+  driver: localForage.LOCALSTORAGE,
+  name: "phex-local"
+});
 
 const acceptedFiles = [".zip", ".ZIP"];
-
-const data = [
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six"
-];
 
 const PheExToaster = Toaster.create({
   className: "toaster",
   position: Position.TOP
 });
 
-const PhenotypeItem = (item, index) => (
-  <div key={index} className="phenotypes__list__item">
-    {item}
-  </div>
-);
+const PhenotypeItem = (item, index) => {
+  return (
+    <div key={index} className="phenotypes__list__item">
+      <span className="phenotypes__list__item__name">
+        <Icon icon="small-plus" />
+        <a href="#">{item.name}</a>
+      </span>
+      <span className="phenotypes__list__item__size">
+        {numeral(item.size).format("0.0 b")}
+      </span>
+    </div>
+  );
+};
 
 PhenotypeItem.propTypes = {
   item: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired
-};
-
-const handleUpload = () => {
-  console.log("uploading...");
 };
 
 const DropArea = (getRootProps, getInputProps, isDragActive) => (
@@ -60,23 +57,51 @@ class AddPhenotype extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPhenotopes: []
+      queue: []
     };
   }
 
-  addFiles(files) {
-    const newSelected = this.state.selectedPhenotopes.concat(files);
-
-    this.setState({
-      selectedPhenotopes: newSelected
-    });
+  queueFiles(files) {
+    const queue = this.state.queue.concat(files);
+    this.setState({ queue });
   }
 
-  removeFile(file) {}
+  async handleUpload() {
+    const files = this.state.queue;
+
+    const transformFiles = files =>
+      files.map(file => {
+        return {
+          modified: file.lastModified,
+          name: file.name,
+          size: file.size
+        };
+      });
+
+    let phenotypes = await localForage.getItem("phenotypes");
+
+    if (phenotypes == null) {
+      phenotypes = transformFiles(files);
+    } else {
+      phenotypes = phenotypes.concat(transformFiles(files));
+    }
+
+    localForage.setItem("phenotypes", phenotypes);
+
+    // clear the queue
+    this.setState({
+      queue: []
+    });
+
+    // close the dialog
+    this.props.onCancel();
+
+    // reload from localStorage
+    this.props.reloadStorage();
+  }
 
   onDrop(acceptedFiles, rejectedFiles) {
-    // this.addFiles(acceptedFiles);
-    console.log(acceptedFiles);
+    this.queueFiles(acceptedFiles);
 
     if (rejectedFiles.length) {
       PheExToaster.show({
@@ -92,15 +117,19 @@ class AddPhenotype extends React.PureComponent {
 
     return (
       <div className="addPhenotype">
-        <Dropzone onDrop={this.onDrop} accept={acceptedFiles}>
+        <Dropzone onDrop={this.onDrop.bind(this)} accept={acceptedFiles}>
           {({ getRootProps, getInputProps, isDragActive }) => {
             return DropArea(getRootProps, getInputProps, isDragActive);
           }}
         </Dropzone>
-        <div className="selectedPhenotypes" />
+        <PhenotypeUploadQueue queue={this.state.queue} />
         <div className="addPhenotype__actions">
           <Button onClick={onCancel}>Cancel</Button>
-          <Button intent="primary" icon="upload" onClick={handleUpload}>
+          <Button
+            intent="primary"
+            icon="upload"
+            onClick={this.handleUpload.bind(this)}
+          >
             Upload
           </Button>
         </div>
@@ -109,12 +138,67 @@ class AddPhenotype extends React.PureComponent {
   }
 }
 
+const PhenotypeUploadQueue = props => {
+  if (props.queue.length === 0) {
+    return null;
+  }
+
+  const items = props.queue.map((file, i) => (
+    <li key={i}>
+      <span className="addPhenotype__selected__phenotypes__name">
+        {file.name}
+      </span>
+      <span className="addPhenotype__selected__phenotypes__size">
+        {numeral(file.size).format("0.0 b")}
+      </span>
+      <span className="addPhenotype__selected__phenotypes__modified">
+        Modified {moment(file.lastModified).format("lll")}
+      </span>
+    </li>
+  ));
+
+  return (
+    <div className="addPhenotype__selected">
+      <h5 className="bp3-heading addPhenotype__selected__title">
+        SELECTED FILES
+      </h5>
+      <div className="addPhenotype__selected__phenotypes">
+        <ul className="bp3-list">{items}</ul>
+      </div>
+    </div>
+  );
+};
+
+const PhenotypeList = props => {
+  const data = props.phenotypes ? props.phenotypes : [];
+
+  return (
+    <div className="phenotypes__list">
+      {data.map((item, index) => PhenotypeItem(item, index))}
+    </div>
+  );
+};
+
 class Phenotypes extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      modalOpen: false
+      modalOpen: false,
+      phenotypes: []
     };
+  }
+
+  reloadStorage() {
+    // Hack until we have a backend
+    localForage.getItem("phenotypes").then(data => {
+      this.setState({
+        phenotypes: data
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.reloadStorage();
   }
 
   render() {
@@ -127,12 +211,11 @@ class Phenotypes extends React.PureComponent {
           }}
           addText="Add"
         />
-        <div className="phenotypes__list">
-          {data.map((item, index) => PhenotypeItem(item, index))}
-        </div>
+        <PhenotypeList phenotypes={this.state.phenotypes} />
         <Dialog
           isOpen={this.state.modalOpen}
           title="UPLOAD PHENOTYPE"
+          style={{ width: "800px" }}
           onClose={() => {
             this.setState({ modalOpen: false });
           }}
@@ -141,6 +224,7 @@ class Phenotypes extends React.PureComponent {
             onCancel={() => {
               this.setState({ modalOpen: false });
             }}
+            reloadStorage={this.reloadStorage.bind(this)}
           />
         </Dialog>
       </div>
--- src/ui/jsx/index.jsx
+++ src/ui/jsx/index.jsx
@@ -1,3 +1,4 @@
+import "babel-polyfill";
 import React from "react";
 import App from "./containers/app/App.jsx";
 

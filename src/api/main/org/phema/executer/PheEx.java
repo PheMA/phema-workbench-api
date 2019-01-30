b1c084bda936df6c3b936ca04511f84a1233bc06
Pascal Brandt <psbrandt@gmail.com>
2019-01-29 20:39:52 -0800

    🧞‍♂️ Add CORS filter.

--- src/api/main/org/phema/executer/PheEx.java
+++ src/api/main/org/phema/executer/PheEx.java
@@ -1,15 +1,19 @@
 package org.phema.executer;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PheEx {
   public static void main(String[] args) {
@@ -28,12 +32,19 @@ public class PheEx {
     servletContextHandler.setContextPath("/");
     servletContextHandler.addServlet(servlet, "/api/v1/*");
 
+    // Add CORS filter
+    FilterHolder cors = servletContextHandler.addFilter(CrossOriginFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+    cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
+
     // Set up the UI servlet
     ServletHolder uiServlet = new ServletHolder("/", DefaultServlet.class);
     uiServlet.setInitParameter("resourceBase", "./src/ui/dist");
-    uiServlet.setInitParameter("pathInfoOnly","true");
-    uiServlet.setInitParameter("dirAllowed","true");
-    servletContextHandler.addServlet(uiServlet,"/*");
+    uiServlet.setInitParameter("pathInfoOnly", "true");
+    uiServlet.setInitParameter("dirAllowed", "true");
+    servletContextHandler.addServlet(uiServlet, "/*");
 
     server.setHandler(servletContextHandler);
 

a4dd359d4bba62cbae57896e9e39b777f3357ce8
Pascal Brandt <psbrandt@gmail.com>
2019-11-10 16:46:24 -0800

    🦟 Bump log level to DEBUG.

--- src/api/main/org/phema/executer/PheEx.java
+++ src/api/main/org/phema/executer/PheEx.java
@@ -21,69 +21,64 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PheEx {
-  public static void main(String[] args) {
-    try
-    {
-      new PheEx().run();
+    public static void main(String[] args) {
+        try {
+            new PheEx().run();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
-    catch (Throwable t)
-    {
-      t.printStackTrace();
+
+    public void run() throws Exception {
+        System.setProperty("org.eclipse.jetty.LEVEL", "DEBUG");
+
+        Server server = new Server(8083);
+        HandlerList handlers = new HandlerList();
+
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.setContextPath("/");
+
+        // Set up the API servlet
+        ResourceConfig config = new ResourceConfig();
+        config.register(JacksonFeature.class);
+        config.packages("org.phema.executer.api.resources");
+
+        ServletHolder apiServletHolder = new ServletHolder(new ServletContainer(config));
+        servletContextHandler.addServlet(apiServletHolder, "/api/v1/*");
+
+        // Set up UI servlet
+        URL webRootLocation = this.getClass().getResource("/ui/index.html");
+        if (webRootLocation == null) {
+            throw new IllegalStateException("Unable to determine webroot URL location");
+        }
+        URI webRootUri = URI.create(webRootLocation.toURI().toASCIIString().replaceFirst("/index.html$", "/"));
+        System.err.printf("Web Root URI: %s%n", webRootUri);
+
+        servletContextHandler.setBaseResource(Resource.newResource(webRootUri));
+        servletContextHandler.setWelcomeFiles(new String[]{"index.html"});
+
+        ServletHolder staticHolder = new ServletHolder("default", DefaultServlet.class);
+        servletContextHandler.addServlet(staticHolder, "/");
+
+        // Add CORS filter
+        FilterHolder cors = servletContextHandler.addFilter(CrossOriginFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
+
+        handlers.addHandler(servletContextHandler);
+        handlers.addHandler(new DefaultHandler());
+
+        server.setHandler(handlers);
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Logger.getLogger(PheEx.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            server.destroy();
+        }
     }
-  }
-
-  public void run() throws Exception
-  {
-    System.setProperty("org.eclipse.jetty.LEVEL", "INFO");
-
-    Server server = new Server(8083);
-    HandlerList handlers = new HandlerList();
-
-    ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    servletContextHandler.setContextPath("/");
-
-    // Set up the API servlet
-    ResourceConfig config = new ResourceConfig();
-    config.register(JacksonFeature.class);
-    config.packages("org.phema.executer.api.resources");
-
-    ServletHolder apiServletHolder = new ServletHolder(new ServletContainer(config));
-    servletContextHandler.addServlet(apiServletHolder, "/api/v1/*");
-
-    // Set up UI servlet
-    URL webRootLocation = this.getClass().getResource("/ui/index.html");
-    if (webRootLocation == null)
-    {
-      throw new IllegalStateException("Unable to determine webroot URL location");
-    }
-    URI webRootUri = URI.create(webRootLocation.toURI().toASCIIString().replaceFirst("/index.html$","/"));
-    System.err.printf("Web Root URI: %s%n",webRootUri);
-
-    servletContextHandler.setBaseResource(Resource.newResource(webRootUri));
-    servletContextHandler.setWelcomeFiles(new String[] { "index.html" });
-
-    ServletHolder staticHolder = new ServletHolder("default", DefaultServlet.class);
-    servletContextHandler.addServlet(staticHolder, "/");
-
-    // Add CORS filter
-    FilterHolder cors = servletContextHandler.addFilter(CrossOriginFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-    cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-    cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
-    cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
-    cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
-
-    handlers.addHandler(servletContextHandler);
-    handlers.addHandler(new DefaultHandler());
-
-    server.setHandler(handlers);
-
-    try {
-      server.start();
-      server.join();
-    } catch (Exception ex) {
-      Logger.getLogger(PheEx.class.getName()).log(Level.SEVERE, null, ex);
-    } finally {
-      server.destroy();
-    }
-  }
 }

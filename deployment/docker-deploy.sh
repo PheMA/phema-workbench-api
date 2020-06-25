fd42ddbccc65920566b0785a8475f0259b588d5a
Pascal Brandt <psbrandt@gmail.com>
2020-06-25 15:47:48 -0700

    🎣 Update Slack webhook.

--- deployment/docker-deploy.sh
+++ deployment/docker-deploy.sh
@@ -10,4 +10,4 @@ docker push phema/phema-workbench-api:$TRAVIS_TAG
 
 # Notify to slack
 SLACK_MESSAGE="New PhEMA Workbench API <https://hub.docker.com/r/phema/phema-workbench-api|Docker image> published ($TRAVIS_TAG)"
-curl -X POST --data-urlencode 'payload={"username": "PhEMA Bot", "text": "'"$SLACK_MESSAGE"'", "as_user": false, "icon_url": "https://avatars2.githubusercontent.com/u/57332201?s=200"}' $SLACK_WEBHOOK_URL
+curl -X POST --data-urlencode 'payload={"text": "'"$SLACK_MESSAGE"'"}' $SLACK_WEBHOOK_URL
